2a9852dc96f3c2d40c8fd80a140348a0dc7110c6
jedlyq <48463129+jedlyq@users.noreply.github.com>
2025-10-06 22:07:55 +0800

    Update commands.js

--- olk/commands.js
+++ olk/commands.js
@@ -1,3 +1,8 @@
+const ssnRegex = /\b(\d{3}-\d{2}-\d{4}|\d{9})\b/g; // /r[e3]views/g;
+const nricRegex = /[SFTG]\d{7}[A-Z]/gm;
+const creditcardRegex = /(\d{4}[-]){3}\d{4}|\d{16}/gm;
+
+
 Office.onReady((info) => {
   console.info('Commands.js::onReady()');
 });
@@ -6,6 +11,43 @@ Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
 
 function onMessageSendHandler(event) {
   console.log("[Commands.js::onMessageSendHandler()] The OnMessageSend event was triggered!");
+
+
+  // ---- SUBJECT ----
+    Office.context.mailbox.item.subject.getAsync(
+      function (asyncResult) {
+        if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+          const subjectHtml = asyncResult.value;
+          console.log("[ARG] SUBJECT -->");
+          console.log("[ARG] " + subjectHtml);
+          console.log("[ARG] <-- SUBJECT");
+          
+          // More robust regex pattern for SSNs
+          // const ssnRegex = /\b(\d{3}-\d{2}-\d{4}|\d{9})\b/g;
+          
+          // Replace matched SSNs with a masked version
+          let sanitizedSubjectHtml = subjectHtml.replace(nricRegex, 'X0000000X');
+          sanitizedSubjectHtml = sanitizedSubjectHtml.replace(creditcardRegex, 'xxxx-xxxx-xxxx-xxxx');
+          console.log("Commands.js::onReady() CLEAN SUBJECT -->");
+          console.log("Commands.js::onReady() " + sanitizedSubjectHtml);
+          console.log("Commands.js::onReady() <-- CLEAN SUBJECT");
+
+          // Set the sanitized HTML back into the email body
+          Office.context.mailbox.item.subject.setAsync(sanitizedSubjectHtml, { coercionType: Office.CoercionType.subjectHtml }, function (setAsyncResult) {
+            if (setAsyncResult.status === Office.AsyncResultStatus.Succeeded) {
+              console.log("Commands.js::onReady() S-1.001 SSNs have been redacted from the email subject.");
+            } else {
+              console.error("Commands.js::onReady() E-1.002 Failed to set the redacted email subject.");
+            }
+          });
+          } else {
+            console.error("Commands.js::onReady() E-1.001 Failed to get subject: " + asyncResult.error.message);
+          }
+      }
+    );
+
+
+  
 
   event.completed({ allowEvent: false });
 }
