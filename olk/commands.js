79c9f9569edef369dd6cfc3af248ae2bcfdd8e65
jedlyq <48463129+jedlyq@users.noreply.github.com>
2025-10-06 22:04:15 +0800

    Update commands.js

--- olk/commands.js
+++ olk/commands.js
@@ -1,13 +1,13 @@
 Office.onReady((info) => {
   console.info('Commands.js::onReady()');
 });
-/*
+
 Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
 
 function onMessageSendHandler(event) {
-  console.log("[Commands.js::onMessageSendHandler] The OnMessageSend event was triggered!");
+  console.log("[Commands.js::onMessageSendHandler()] The OnMessageSend event was triggered!");
 
   event.completed({ allowEvent: false });
 }
-*/
+
 
