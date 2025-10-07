15c2cea437af483590210498822fcfed4cba1177
jedlyq <48463129+jedlyq@users.noreply.github.com>
2025-10-07 15:57:23 +0800

    Update commands.js

--- olk/commands.js
+++ olk/commands.js
@@ -1,6 +1,11 @@
-const ssnRegex = /\b(\d{3}-\d{2}-\d{4}|\d{9})\b/g; // /r[e3]views/g;
+const ssnRegex = /\b(\d{3}-\d{2}-\d{4}|\d{9})\b/g;
 const nricRegex = /[SFTG]\d{7}[A-Z]/gm;
 const creditcardRegex = /(\d{4}[-]){3}\d{4}|\d{16}/gm;
+
+
+
+const nricRedacted = 'X0000000X';
+const creditcardRedacted = 'xxxx-xxxx-xxxx-xxxx';
 
 
 Office.onReady((info) => {
@@ -9,47 +14,128 @@ Office.onReady((info) => {
 
 Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
 
+// Factories
+const makePromiseSetSubject = (mailItem, newSubject) => {
+  return new Promise((resolve, reject) => {
+    console.log("[ARG] trying to set subject to [" + newSubject + "]");
+    mailItem.subject.setAsync(newSubject, { coercionType: Office.CoercionType.subjectHtml }, function (setAsyncResult) {
+      if (setAsyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.log("[ARG] set subject OK" + setAsyncResult.value);
+        resolve(setAsyncResult.value);
+      } else {
+        console.log("[ARG] set subject BAD" + setAsyncResult.error.message);
+        reject(setAsyncResult.error.message);
+      }
+    });
+  });
+};
+
+const makePromiseSetBody = (mailItem, newBody) => {
+  return new Promise((resolve, reject) => {
+    console.log("[ARG] trying to set body to [" + newBody + "]");
+    mailItem.body.setAsync(newBody, { coercionType: Office.CoercionType.Html }, function (setAsyncResult) {
+      if (setAsyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.log("[ARG] set body OK" + setAsyncResult.value);
+        resolve(setAsyncResult.value);
+      } else {
+        console.log("[ARG] set body BAD" + setAsyncResult.error.message);
+        reject(setAsyncResult.error.message);
+      }
+    });
+  });
+};
+
+
+// event handler
 function onMessageSendHandler(event) {
   console.info("[Commands.js::onMessageSendHandler()] Received OnMessageSend event!");
 
 
-  // ---- SUBJECT ----
-    Office.context.mailbox.item.subject.getAsync(
-      function (asyncResult) {
-        if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-          const subjectHtml = asyncResult.value;
-          console.info("[ARG] SUBJECT -->");
-          console.info("[ARG] " + subjectHtml);
-          console.info("[ARG] <-- SUBJECT");
-          
-          // More robust regex pattern for SSNs
-          // const ssnRegex = /\b(\d{3}-\d{2}-\d{4}|\d{9})\b/g;
-          
-          // Replace matched SSNs with a masked version
-          let sanitizedSubjectHtml = subjectHtml.replace(nricRegex, 'X0000000X');
-          sanitizedSubjectHtml = sanitizedSubjectHtml.replace(creditcardRegex, 'xxxx-xxxx-xxxx-xxxx');
-          console.info("Commands.js::onReady() CLEAN SUBJECT -->");
-          console.info("Commands.js::onReady() " + sanitizedSubjectHtml);
-          console.info("Commands.js::onReady() <-- CLEAN SUBJECT");
+const item = Office.context.mailbox.item;
+  let sanitizedSubjectHtml = "";
+  let sanitizedBodyHtml = "";
 
-          // Set the sanitized HTML back into the email body
-          Office.context.mailbox.item.subject.setAsync(sanitizedSubjectHtml, { coercionType: Office.CoercionType.subjectHtml }, function (setAsyncResult) {
-            if (setAsyncResult.status === Office.AsyncResultStatus.Succeeded) {
-              console.info("[Commands.js::onMessageSendHandler()] S-1.001 SSNs have been redacted from the email subject.");
-
-              event.completed({allowEvent: false, errorMessage: "Redacted! But I still block you!!!"});
-            } else {
-              console.info("[Commands.js::onMessageSendHandler()] E-1.002 Failed to set redacted subject: " + asyncResult.error.message);
-
-              event.completed({allowEvent: false, errorMessage: "Not able to set redacted SUBJECT!!!"});
-            }
-          });
-          } else {
-            console.info("[Commands.js::onMessageSendHandler()] E-1.001 Failed to get subject: " + asyncResult.error.message);
-            event.completed({ allowEvent: false, errorMessage: "Not able to retrieve SUBJECT!!!"});
-          }
+  const getSubjectPromise = new Promise((resolve, reject) => {
+    console.log("[ARG] trying to get subject");
+    item.subject.getAsync((asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.log("[ARG] fetch subject OK");
+        resolve(asyncResult.value);
+      } else {
+        console.log("[ARG] fetch subject BAD");
+        reject(asyncResult.error.message);
       }
-    );
+    });
+  });
+
+  const getBodyPromise = new Promise((resolve, reject) => {
+    console.log("[ARG] trying to get body");
+    item.body.getAsync(Office.CoercionType.Html, (asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.log("[ARG] fetch body OK");
+        resolve(asyncResult.value);
+      } else {
+        console.log("[ARG] fetch body BAD");
+        reject(asyncResult.error.message);
+      }
+    });
+  });
+
+
+
+    
+    Promise.all([getSubjectPromise, getBodyPromise]).then(([subjectHtml, bodyHtml]) => {
+        subjectHtml = subjectHtml + "";
+        subjectHtml = subjectHtml.trim();
+        bodyHtml = bodyHtml + "";
+        bodyHtml = bodyHtml.trim();
+
+        console.log("[ARG] SUBJECT --->");
+        console.log("[ARG] " + subjectHtml);
+        console.log("[ARG] <--- SUBJECT");
+
+        console.log("[ARG] BODY --->");
+        console.log("[ARG] " + bodyHtml);
+        console.log("[ARG] <--- BODY");
+
+        sanitizedSubjectHtml = subjectHtml.replace(nricRegex, nricRedacted);
+        sanitizedSubjectHtml = sanitizedSubjectHtml.replace(creditcardRegex, creditcardRedacted);
+
+        sanitizedBodyHtml = bodyHtml.replace(nricRegex, nricRedacted);
+        sanitizedBodyHtml = sanitizedBodyHtml.replace(creditcardRegex, creditcardRedacted);
+
+        console.log("[ARG] Sanitized SUBJECT --->");
+        console.log("[ARG] " + sanitizedSubjectHtml);
+        console.log("[ARG] <--- Sanitized SUBJECT");
+
+        console.log("[ARG] Sanitized BODY --->");
+        console.log("[ARG] " + sanitizedBodyHtml);
+        console.log("[ARG] <--- Sanitized BODY");
+
+        let promiseSetSubject = makePromiseSetSubject(item, sanitizedSubjectHtml);
+        let promiseSetBody = makePromiseSetBody(item, sanitizedBodyHtml);
+
+        Promise.all([promiseSetSubject, promiseSetBody]).then(() => {
+            console.info("[ARG] successfully set redacted SUBJECT / BODY:");
+            event.completed({ allowEvent: false, errorMessage: "Everything OK, but still don't let you send" });
+        }).catch((error) => {
+            console.error("[ARG] An error occurred while setting item data:", error);
+            event.completed({ allowEvent: false, errorMessage: "Not able to set SUBJECT, BODY!!!" });
+        });
+    }).catch((error) => {
+        console.error("[ARG] An error occurred while fetching item data:", error);
+        event.completed({ allowEvent: false, errorMessage: "Not able to retrieve SUBJECT, BODY!!!" });
+    });
+
+
+  
+
+  
+
+  console.info("[Commands.js::onMessageSendHandler()] Exit!");
+}
+
+
 
 
   
