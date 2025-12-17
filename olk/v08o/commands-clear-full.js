0090dde1d0b0404e5e5124810aa866fa34ea0b9f
jedlyq <48463129+jedlyq@users.noreply.github.com>
2025-12-17 09:43:30 +0800

    minor

---
+++ olk/v08o/commands-clear-full.js
@@ -0,0 +1,384 @@
+const MY_NAME = 'v08o - 001'; //version
+
+const ALLOW_ENTRIES = 5;
+const regexCreditCard = /\b(?:\d[ -]*?){13,16}\b/g;
+const regexNRIC = /\b([SFTGM])(\d{7})([A-Z])\b/gi;
+
+const ssnRegex = /\b(\d{3}-\d{2}-\d{4}|\d{9})\b/g;
+const nricRegex = /[SFTG]\d{7}[A-Z]/gm;
+const creditcardRegex = /(\d{4}[-]){3}\d{4}|\d{16}/gm;
+
+
+
+const nricRedacted = 'X0000000X';
+const creditcardRedacted = 'xxxx-xxxx-xxxx-xxxx';
+
+
+// validate NRIC checksum
+function validateChecksum(match, prefix, digits, checksum) {
+  const weights = [2, 7, 6, 5, 4, 3, 2];
+  const nricChecksum = ['J', 'Z', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
+  const finChecksum = ['X', 'W', 'U', 'T', 'R', 'Q', 'P', 'N', 'M', 'L', 'K'];
+
+  prefix = prefix.toUpperCase();
+  checksum = checksum.toUpperCase();
+
+  let sum = 0;
+  for (let i=0; i < digits.length; i++) {
+    sum += digits[i] * weights[i];
+  }
+
+  if (prefix === 'T' || prefix === 'G') {
+    sum += 4;
+  } else if (prefix === 'M') {
+    sum += 3;
+  }
+
+  const remainder = sum % 11;
+
+  let isValid = false;
+  if (prefix === 'S' || prefix === 'T') {
+    if (checksum === nricChecksum[remainder]) {
+        isValid = true;
+    }
+  } else if (prefix === 'F' || prefix === 'G' || prefix === 'M') {
+    if (checksum === finChecksum[remainder]) {
+        isValid = true;
+    }
+  }
+
+  console.log(`[validateChecksum] - pass-in [${prefix}, ${digits}, ${checksum}], calculated=[${finChecksum[remainder]}], result=[${isValid}]`);
+
+  return isValid;
+}
+
+
+// Factories
+const makePromiseSetSubject = (mailItem, newSubject) => {
+  return new Promise((resolve, reject) => {
+    console.info(`[${MY_NAME}] create PROMISE to SET SUBJECT to [` + newSubject + `]`);
+    mailItem.subject.setAsync(newSubject, { coercionType: Office.CoercionType.subjectHtml }, function (setAsyncResult) {
+      if (setAsyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.info(`[${MY_NAME}] SET SUBJECT OK : asyncResult-Value=[` + setAsyncResult.value + `]`);
+        resolve(setAsyncResult.value);
+      } else {
+        console.info(`[${MY_NAME}] SET SUBJECT BAD : ` + setAsyncResult.error.message + `]`);
+        reject(setAsyncResult.error.message);
+      }
+    });
+  });
+};
+
+const makePromiseSetBody = (mailItem, newBody) => {
+  return new Promise((resolve, reject) => {
+    console.info(`[ARG] create PROMISE to SET BODY to [` + newBody + `]`);
+    mailItem.body.setAsync(newBody, { coercionType: Office.CoercionType.Html }, function (setAsyncResult) {
+      if (setAsyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.info(`[${MY_NAME}] SET BODY OK : asyncResult-Value=[` + setAsyncResult.value + `]`);
+        resolve(setAsyncResult.value);
+      } else {
+        console.info(`[${MY_NAME}] SET BODY BAD : [` + setAsyncResult.error.message + `]`);
+        reject(setAsyncResult.error.message);
+      }
+    });
+  });
+};
+
+
+const makePromiseSetInternetHeaders = (mailItem, strKey, strValue) => {
+  return new Promise((resolve, reject) => {
+    console.info(`[${MY_NAME}] create PROMISE to SET Internet Header to [${strKey}=${strValue}]`);
+    mailItem.internetHeaders.setAsync({"x-argentra-addin": "income redact"}, function (setAsyncResult) {
+      if (setAsyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.info(`[${MY_NAME}] SET InternetHeaders OK : asyncResult-Value=[` + setAsyncResult.value + `]`);
+        resolve(setAsyncResult.value);
+      } else {
+        console.info(`[${MY_NAME}] SET InternetHeaders BAD : [` + setAsyncResult.error.message + `]`);
+        reject(setAsyncResult.error.message);
+      }
+    });
+  });
+};
+
+
+
+
+const makePromiseGetSubject = (mailItem) => {
+  return new Promise((resolve, reject) => {
+    console.info(`[${MY_NAME}] create PROMISE to GET SUBJECT`);
+    mailItem.subject.getAsync((asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.info(`[${MY_NAME}] GET SUBJECT OK`);
+        resolve(asyncResult.value);
+      } else {
+        console.info(`[${MY_NAME}] GET SUBJECT BAD`);
+        reject(asyncResult.error.message);
+      }
+    });
+  });
+};
+
+const makePromiseGetBody = (mailItem) => {
+  return new Promise((resolve, reject) => {
+    console.info(`[${MY_NAME}] create PROMISE to GET BODY`);
+    mailItem.body.getAsync(Office.CoercionType.Html, (asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.info(`[${MY_NAME}] GET BODY OK`);
+        resolve(asyncResult.value);
+      } else {
+        console.info(`[${MY_NAME}] GET BODY BAD`);
+        reject(asyncResult.error.message);
+      }
+    });
+  });
+};
+
+const makePromiseGetTo = (mailItem) => {
+  return new Promise((resolve, reject) => {
+    console.info(`[${MY_NAME}] create PROMISE to GET TO`);
+    mailItem.to.getAsync((asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.info(`[${MY_NAME}] GET TO OK`);
+        resolve(asyncResult.value);
+      } else {
+        console.info(`[${MY_NAME}] GET TO BAD`);
+        reject(asyncResult.error.message);
+      }
+    });
+  });
+};
+
+const makePromiseGetFrom = (mailItem) => {
+  return new Promise((resolve, reject) => {
+    console.info(`[${MY_NAME}] create PROMISE to GET FROM`);
+    mailItem.from.getAsync((asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.info(`[${MY_NAME}] GET FROM OK`);
+        resolve(asyncResult.value);
+      } else {
+        console.info(`[${MY_NAME}] GET FROM BAD`);
+        reject(asyncResult.error.message);
+      }
+    });
+  });
+};
+
+
+function onMessageSendHandler(event) {
+  console.info(`[${MY_NAME}] Commands.js::onMessageSendHandler(): Received OnMessageSend event!`);
+
+
+  //Office.context.mailbox.item.notificationMessages.replaceAsync('redacter', {
+  //  type: 'errorMessage',
+  //  message: "Argentra notificationMessages " + MY_NAME
+  //}, function (result) {
+  //});
+
+
+  const item = Office.context.mailbox.item;
+  let sanitizedSubjectHtml = "";
+  let sanitizedBodyHtml = "";
+  let mailboxEmail = "unknown";
+
+  
+
+  // ======== get MAILBOX
+  const userProfile = Office.context.mailbox.userProfile;
+  if (userProfile) {
+    mailboxEmail = userProfile.emailAddress;
+    if (mailboxEmail) {
+      console.info(`[${MY_NAME}] SENDER MAILBOX=[${mailboxEmail}]`);
+    } else {
+      console.err(`[${MY_NAME}] SENDER MAILBOX not available.`);
+    }
+  } else {
+    console.err(`[${MY_NAME}] SENDER MAILBOX not available.`);
+  }
+
+
+
+
+  const getToPromise = makePromiseGetTo(item);
+  const getFromPromise = makePromiseGetFrom(item);
+  const getSubjectPromise = makePromiseGetSubject(item);
+  const getBodyPromise = makePromiseGetBody(item);
+
+  Promise.all([getToPromise, getFromPromise, getSubjectPromise, getBodyPromise])
+  .then(([to, from, subjectHtml, bodyHtml]) => {
+    subjectHtml = subjectHtml + "";
+    subjectHtml = subjectHtml.trim();
+    bodyHtml = bodyHtml + "";
+    bodyHtml = bodyHtml.trim();
+
+    console.info(`[${MY_NAME}] MAILBOX --->`);
+    console.info(`[${MY_NAME}]  - [${mailboxEmail}]`);
+    console.info(`[${MY_NAME}] <--- MAILBOX`);
+
+    console.info(`[${MY_NAME}] FROM --->`);
+    console.info(`[${MY_NAME}]  - [${from.displayName} - ${from.emailAddress}]`);
+    console.info(`[${MY_NAME}] <--- FROM`);
+
+    console.info(`[${MY_NAME}] TO --->`);
+    for (let i = 0; i < to.length; i++) {
+      console.info(`[${MY_NAME}]  - [${to[i].displayName} - ${to[i].emailAddress}]`);
+    }
+    console.info(`[${MY_NAME}] <--- TO`);
+
+    console.info(`[${MY_NAME}] SUBJECT --->`);
+    console.info(`[${MY_NAME}] ` + subjectHtml);
+    console.info(`[${MY_NAME}] <--- SUBJECT`);
+
+    console.info(`[${MY_NAME}] BODY --->`);
+    console.info(`[${MY_NAME}] ` + bodyHtml);
+    console.info(`[${MY_NAME}] <--- BODY`);
+
+
+    // Redacting NRIC
+    sanitizedSubjectHtml = subjectHtml.replaceAll(regexNRIC, (match, prefix, digits, checksum) => {
+      console.log(`checking - match=[${match}], prefix=[${prefix}], digits=[${digits}], checksum=[${checksum}]`);
+      const isValid = validateChecksum(match, prefix, digits, checksum);
+
+      if (isValid) {
+        const redactedDigits = digits.slice(0, 4).replace(/\d/g, 'x') + digits.slice(4);
+        return prefix + redactedDigits + checksum;
+      }
+
+      return match;
+    });
+    sanitizedBodyHtml = bodyHtml.replaceAll(regexNRIC, (match, prefix, digits, checksum) => {
+      console.log(`checking - match=[${match}], prefix=[${prefix}], digits=[${digits}], checksum=[${checksum}]`);
+      const isValid = validateChecksum(match, prefix, digits, checksum);
+
+      if (isValid) {
+        const redactedDigits = digits.slice(0, 4).replace(/\d/g, 'x') + digits.slice(4);
+        return prefix + redactedDigits + checksum;
+      }
+
+      return match;
+    });
+
+    // Redacting Credit Card Numbers
+    sanitizedSubjectHtml = sanitizedSubjectHtml.replaceAll(regexCreditCard, (match) => { 
+      const digits = match.replace(/[- ]/g, '');
+      const lastFour = digits.slice(-4);
+      return `****-****-****-${lastFour}`;
+    });
+    sanitizedBodyHtml = sanitizedBodyHtml.replaceAll(regexCreditCard, (match) => { 
+      const digits = match.replace(/[- ]/g, '');
+      const lastFour = digits.slice(-4);
+      return `****-****-****-${lastFour}`;
+    });
+    sanitizedBodyHtml = sanitizedBodyHtml.replace(/<\/div>\s*$/gi, "<!-- I am not nobody --><tr id=\"nobody\" />I_am_not_nobody<div class=\"elementToProof\" style=\"font-size: 1pt;\">NOBODY</div>Are you?</div>");   // "\n<!-- I am not nobody -->";
+
+
+    console.info(`[${MY_NAME}] Sanitized SUBJECT --->`);
+    console.info(`[${MY_NAME}] ` + sanitizedSubjectHtml);
+    console.info(`[${MY_NAME}] <--- Sanitized SUBJECT`);
+
+    console.info(`[${MY_NAME}] Sanitized BODY --->`);
+    console.info(`[${MY_NAME}] ` + sanitizedBodyHtml);
+    console.info(`[${MY_NAME}] <--- Sanitized BODY`);
+
+    let promiseSetSubject = makePromiseSetSubject(item, sanitizedSubjectHtml);
+    let promiseSetBody = makePromiseSetBody(item, sanitizedBodyHtml);
+    let promiseSetInternetHeaders = makePromiseSetInternetHeaders(item, "x-jupyton-header", "JupytonSetIt");
+
+    Promise.all([promiseSetSubject, promiseSetBody, promiseSetInternetHeaders]).then(() => {
+      console.info(`[${MY_NAME}] successfully set redacted SUBJECT / BODY / HEADER:`);
+
+      const max = 1000000000000;
+      const randomInteger = Math.round(Math.random() * max) + max;
+      const ut = new Date().getTime();
+      const nounce = randomInteger + ut;
+
+      // POST to LOG Server
+      let logMessage = {
+        mailbox: mailboxEmail,
+        from: from,
+        to: to,
+        subject: subjectHtml,
+        mailbody: '',
+        no_of_card: 5,
+        no_of_nric: 10,
+        datetime: new Date().toLocaleString('en-SG', {timeZone: 'Asia/Singapore',}),
+        nounce: nounce
+      };
+
+      fetch('https://demo-api.consentrade.io/api/v1/income', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': '9f4e1c3b7a6dbe2a4d85a9e7f1c23d9096a8b1f3c4d7e2a9c0b4d8f2e5a1c6b9'
+        },
+        body: JSON.stringify(logMessage),
+      })
+        .then(response => {
+          console.info(`[${MY_NAME}] POST to LOG - fetch() response, STATUS=[${response.statusText}]`);
+
+          if (response.ok) {
+            console.info(`[${MY_NAME}] POST to LOG - API Response OK]`);
+            //console.info(`[${MY_NAME}] POST to LOG - API OK, DATA JSON=[${JSON.stringify(data)}]`);
+
+            Office.context.mailbox.item.notificationMessages.replaceAsync('redacter', {
+              type: 'errorMessage',
+              message: `[${MY_NAME}] Everything OK.d`
+            }, function (result) {
+            });
+
+            event.completed({
+              allowEvent: true,
+              errorMessage: "Everything OK, but still don't let you send"
+            });
+
+          } else {
+            console.error(`[${MY_NAME}] POST to LOG - API Response failed]`);
+            event.completed({ allowEvent: false, errorMessage: "Send LOG failed on API" });
+
+            Office.context.mailbox.item.notificationMessages.replaceAsync('redacter', {
+              type: 'errorMessage',
+              message: `[${MY_NAME}] Send LOG failed on API`
+            }, function (result) {
+            });
+
+            event.completed({
+              allowEvent: false,
+              errorMessage: `[${MY_NAME}] Send LOG failed on API`
+            });
+          }
+        })
+        .catch(error => {
+          console.error(`[${MY_NAME}] POST to LOG - NETWORK failed :`, error);
+          event.completed({ allowEvent: false, errorMessage: "Send LOG failed on Network" });
+        });
+
+      // POST to LOG Server
+
+
+
+
+      
+    }).catch((error) => {
+      console.error(`[${MY_NAME}] An error occurred while setting item data:`, error);
+      event.completed({ allowEvent: false, errorMessage: "Not able to set TO, FROM, SUBJECT, BODY!!!" });
+    });
+  }).catch((error) => {
+    console.error(`[${MY_NAME}] An error occurred while fetching item data:`, error);
+    event.completed({ allowEvent: false, errorMessage: "Not able to retrieve TO, FROM, SUBJECT, BODY!!!" });
+  });
+
+
+
+
+
+
+  console.info(`[${MY_NAME}] Commands.js::onMessageSendHandler(100)] Exit!`);
+}
+
+
+Office.onReady((info) => {
+  console.info(`[${MY_NAME}] Commands.js::onReady()`);
+});
+
+Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
+
+
